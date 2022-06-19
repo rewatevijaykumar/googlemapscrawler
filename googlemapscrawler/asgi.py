ea1bf7735b707f63d647a3d1924145ad21719c9b
dataeons <dataeons@gmail.com>
2022-06-19 13:07:28 +0530

    initial

--- googlemapscrawler/asgi.py
+++ googlemapscrawler/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for googemapscrawler project.
+ASGI config for googlemapscrawler project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'googemapscrawler.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'googlemapscrawler.settings')
 
 application = get_asgi_application()
